f566498577719c939b34f599f67e7e02a587f140
Alex <gurievalex78@gmail.com>
2020-11-09 16:52:51 +0100

    erf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Testo prova
+                    Modifica
                 </div>
 
                 <div class="links">

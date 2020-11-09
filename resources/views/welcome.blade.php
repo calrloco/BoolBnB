ae0e7f89d9aa102082ef48fcf0827ed5dad7e20d
Alex <gurievalex78@gmail.com>
2020-11-09 16:50:15 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Testo prova
                 </div>
 
                 <div class="links">

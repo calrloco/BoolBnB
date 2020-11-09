8e18c9ebb4dec2dc4664978860c5e54ed0156fc4
carlo briganti <505641nl@saeinstitute.edu>
2020-11-09 15:20:26 +0100

    primo push

---
+++ resources/views/test.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <nav class="nav-container">
+            <div class="container-center">
+                <div class="nav">
+                    <div class="nav__logo">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1024px-Airbnb_Logo_B%C3%A9lo.svg.png"
+                            alt="">
+                    </div>
+                    <div class="nav__search">
+                        <button class="nav__search-button">
+                            Inizia la ricerca
+                            <div class="nav__search-icon">
+
+                            </div>
+                        </button>
+                    </div>
+                    <div class="nav__user">
+                        <div class="nav__user-hosting">
+                            <a href="#">Diventa un Host</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</body>
+
+</html>

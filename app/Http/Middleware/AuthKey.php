38eeaff765e0ea9c67030d9c7fee0eeda2f2aab7
carlo briganti <505641nl@saeinstitute.edu>
2020-11-12 15:34:19 +0100

    API APPARTAMENTO CRUD API KEY

---
+++ app/Http/Middleware/AuthKey.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AuthKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = $request->header('KEY');
+        if($token == 'test'){
+            return $next($request);
+        }else{
+            return response()->json(['messaggio'=>'chiave non ricosciuta']);
+        }
+       
+    }
+}

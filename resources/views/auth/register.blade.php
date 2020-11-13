c025c4e23887d016dd2fc1ea69c6bd1e5246fbdf
yuridido <66362431+yuridido@users.noreply.github.com>
2020-11-13 16:14:55 +0100

    register sistemato

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
-
+                    BOOLEAN
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
@@ -24,6 +24,10 @@
                                 @enderror
                             </div>
                         </div>
+
+                        <input type="text" name='lastname' placeholder='lastname'>
+                        <input type="date" name='date_of_birth'>
+
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>

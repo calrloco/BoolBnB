86a6115844217d2b42bf90d67573a2127172d12c
carlo briganti <505641nl@saeinstitute.edu>
2020-11-14 01:18:59 +0100

    home page prima stilizzazione generale

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,59 +1,29 @@
 @extends('layouts.app')
-
 @section('content')
 
     <header class="jumbotron">
-        <div class="container-center">
+        <div class="container-center container-fullheight">
             <div class="jumbotron__text">
-                <p class="jumbotron__text-banner">Entra in airbnb</p>
+                <p class="jumbotron__text-banner">Vicino e bello</p>
                 <button class="jumbotron__text-button">Esplora i soggiorni nei dintorni</button>
             </div>
         </div>
     </header>
     <div class="container-center">
         <section class="highlited">
-            <i class="fas fa-star"></i>
             <p class="sponsor__home-title">In evidenza</p>
             <div class="sponsor__home">
-
-                <div class="sponsor__home-card">
-                    <div class="sponsor__home-card-img">
-                        <img src="https://a0.muscache.com/im/pictures/a0316ecb-e49b-4b3a-b6b6-c2876b820e8c.jpg?im_w=720"
-                            alt="">
+                @for ($i = 0; $i < 4; $i++)
+                    <div class="sponsor__home-card">
+                        <div class="sponsor__home-card-img">
+                            <img src="https://a0.muscache.com/im/pictures/a0316ecb-e49b-4b3a-b6b6-c2876b820e8c.jpg?im_w=720"
+                                alt="">
+                        </div>
+                        <div class="sponsor__home-card-text">
+                            <p>{{ $apartment[$i]->title }}</p>
+                        </div>
                     </div>
-                    <div class="sponsor__home-card-text">
-                        <p>Lorem ipsum dolor sit amet consectetu</p>
-                    </div>
-                </div>
-                <div class="sponsor__home-card">
-                    <div class="sponsor__home-card-img">
-                        <img src="https://a0.muscache.com/im/pictures/a0316ecb-e49b-4b3a-b6b6-c2876b820e8c.jpg?im_w=720"
-                            alt="">
-                    </div>
-                    <div class="sponsor__home-card-text">
-                        <p>Lorem ipsum dolor sit amet consectetu</p>
-                    </div>
-                </div>
-                <div class="sponsor__home-card">
-                    <div class="sponsor__home-card-img">
-                        <img src="https://a0.muscache.com/im/pictures/a0316ecb-e49b-4b3a-b6b6-c2876b820e8c.jpg?im_w=720"
-                            alt="">
-                    </div>
-                    <div class="sponsor__home-card-text">
-                        <p>Lorem ipsum dolor sit amet consectetu</p>
-                    </div>
-                </div>
-                <div class="sponsor__home-card">
-                    <div class="sponsor__home-card-img">
-                        <img src="https://a0.muscache.com/im/pictures/a0316ecb-e49b-4b3a-b6b6-c2876b820e8c.jpg?im_w=720"
-                            alt="">
-                    </div>
-                    <div class="sponsor__home-card-text">
-                        <p>Lorem ipsum dolor sit amet consectetu</p>
-                    </div>
-                </div>
-            </div>
-
+                @endfor
         </section>
     </div>
     {{-- <div class="row justify-content-center">

35ec8d348912e5295bb329edcc48b508073e4b54
carlo briganti <505641nl@saeinstitute.edu>
2020-11-16 11:13:56 +0100

    views

--- app/Http/Controllers/ViewsController.php
+++ app/Http/Controllers/ViewsController.php
@@ -3,11 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\View;
-use App\Apartment;
-use App\User;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
@@ -20,9 +17,9 @@ class ViewsController extends Controller
      */
     public function show($id)
     {
-        $views =  DB::table('views')->selectRaw('DATE(views.created_at),COUNT(DATE(views.created_at))')
+        $views =  DB::table('views')->selectRaw('DATE(views.created_at),COUNT(DATE(views.created_at)) as daily_views')
             ->join('apartments', 'views.apartment_id', '=', 'apartments.id')
-            ->join('users', 'apartments.user_id', '=', 'users.id')->where('apartment_id', '=', $id)
+            ->where('apartment_id', '=', $id)
             ->groupBy('views.created_at')->get();
             $count = count($views);
             return view('test',compact('views'));
@@ -36,17 +33,22 @@ class ViewsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request,$id)
     {
-
+         
+        // parte sotre chiedo l'ip del client
         $clientIp = $request->ip();
+        // creo una variabile per controllere se esiste il dato con questo ip alla data di oggi
         $viewCheck = View::where('ip_guest', '=', $clientIp)
-            ->whereRaw('DATE(`views.created_at`) = CURRENT_DATE')->get();
+            ->whereRaw('DATE(`views.created_at`) = CURRENT_DATE')
+            ->where('apartment_id','=',$id)
+            ->get();
         if (count($viewCheck) < 1) {
-            $validator = Validator::make($request->all(), [
-                'apartment_id' => 'required'
-            ]);
-            View::create(array_merge($request->all(), ['ip_guest' => $clientIp]));
+            $validator =  [
+                'apartment_id' => $id,
+                'ip_guest' => $clientIp
+            ];
+            View::create($validator);
         }
     }
 }

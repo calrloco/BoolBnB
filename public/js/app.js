47fd68d2004e70da5d51473205f63d29611b355e
carlo briganti <505641nl@saeinstitute.edu>
2020-11-18 16:03:06 +0100

    stats js

--- public/js/app.js
+++ public/js/app.js
@@ -42377,49 +42377,6 @@ function hidenav() {
   $(".nav__search-button").addClass("nav__search-button-large");
   $(".nav__search-icon-big").addClass("active-flex");
   $("#start-search").addClass("hidden");
-} //// statistiche
-
-
-var myfunction = function () {
-  var labels = [];
-  var dataSet = [];
-  $.ajax({
-    url: 'http://127.0.0.1:8000/api/stats',
-    method: 'GET',
-    headers: {
-      KEY: 'test'
-    },
-    data: {
-      id: '7'
-    },
-    success: function success(response) {
-      for (var i = 0; i < response.length; i++) {
-        labels.push(response[i].date);
-        dataSet.push(response[i].daily_views);
-      }
-
-      console.log(labels, dataSet);
-      compileChart(labels, dataSet);
-    },
-    error: function error() {
-      console.log('connessione non riuscita');
-    }
-  });
-}();
-
-function compileChart(label, dataset) {
-  var views = $('#chart');
-  var statChart = new Chart(views, {
-    type: "line",
-    data: {
-      labels: label,
-      datasets: [{
-        label: 'visualizzazioni',
-        data: dataset
-      }]
-    },
-    options: {}
-  });
 }
 
 /***/ }),

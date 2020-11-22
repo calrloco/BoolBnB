873714ce789b087025be18d20750277e8d4fdc78
yuridido <yuridido@yahoo.it>
2020-11-22 10:54:20 +0100

    messaggi ok

--- resources/views/logged/messages.blade.php
+++ resources/views/logged/messages.blade.php
@@ -1,27 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- @dd($messages) --}}
+
 <div class="container-center">
-<a href="{{ route(''host.index'')}}">vedi tuoi appartamenti</a>
+    {{-- avviso di riuscita eliminazione messaggio --}}
+    @if (session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+    @endif
+    
+    {{-- controllo presenza di messaggi --}}
     @if (!empty($messages)) 
     <h2>I tuoi messaggi</h2>
     <div class=messages-deck>
         
 
         @foreach($messages as $message)
-        <div class="message-card">
+    <div class="message-card {{ (($message->read == 0) ? 'unread' : 'read') }}">
             <div class="apart-info">
                 <div class=apart-img>
-                    
+                <img src="{{ $message->apartment->images[0]->path }}" alt="foto appartamento">
                 </div>
                 <div>
-                    <p>{{ $message->title }}</p>
+                    <p>{{ $message->apartment->title }}</p>
                 </div>
             </div>
             <div class="message-info">
                 <p> <strong>Da: </strong> {{ $message->email }}</p>
                 <p> <strong>Messaggio: </strong> {{ $message->message }}</p>
+            </div>
+            <div class="button-section">
+                {{-- controllo messaggio letto --}}
+                @If($message->read == 0) 
+                <form action="{{ route('messages.update', $message->id_msg) }}" method="post">
+                    @csrf
+                    @method('PATCH')
+                    <button type="submit"><i class="fas fa-envelope"></i></button>
+                </form>
+                @else
+                <form action="{{ route('messages.update', $message->id_msg) }}" method="post">
+                    @csrf
+                    @method('PATCH')
+                    <button type="submit"><i class="fas fa-envelope-open"></i></button>
+                </form>
+                @endif
+
+                <form action="{{ route('messages.destroy', $message->id_msg) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit"><i class="fas fa-trash-alt"></i></button>
+                </form>
             </div>
         </div>
         @endforeach   

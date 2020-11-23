ed28fda16a49c787a7cd962847e54b6beb02810d
Alex <gurievalex78@gmail.com>
2020-11-23 19:30:41 +0100

    Pagamento OK

--- resources/js/sponsor.js
+++ resources/js/sponsor.js
@@ -1,19 +1,63 @@
 
-$('#sponsors-select').on('change',function(){
-    $('#summary-sponsor').empty();
-    var check = $('#sponsors-select').val();
-    var sponsor = JSON.parse(check);
-    var price = sponsor.sponsor_price;
-    $('#summary-sponsor').append(price);
-    console.log(sponsor);
+document.addEventListener("DOMContentLoaded", function () {
+    var form = document.querySelector('#payment-form');
+    var client_token = document.querySelector('#client_token').value;
 
+    var option1 = document.querySelector('#sponsorBasic');
+    var option2 = document.querySelector('#sponsorMedium');
+    var option3 = document.querySelector('#sponsorPremium');
+    var amount = document.querySelector('#amount');
+    var sponsor_plan = document.querySelector('#sponsor_plan');
 
-$('#btn-sponsor').click(function(){
-    // var price = sponsor.sponsor_price;
-    // $('#summary-sponsor').append(price);
-    console.log(price);
+   
+    
+
+    
+
+    option1.onclick = function () {
+        amount.value = option1.value;
+        amount_preview.innerHTML = '€' + amount.value;
+        sponsor_plan.value = 1;
+    };
+
+    option2.onclick = function () {
+        amount.value = option2.value;
+        amount_preview.innerHTML = '€' + amount.value;
+        sponsor_plan.value = 2;
+    };
+
+    option3.onclick = function () {
+        amount.value = option3.value;
+        amount_preview.innerHTML = '€' + amount.value;
+        sponsor_plan.value = 3;
+    };
+
+    braintree.dropin.create({
+        authorization: client_token,
+        selector: '#bt-dropin',
+        paypal: {
+            flow: 'vault'
+        }
+    }, function (createErr, instance) {
+
+        if (createErr) {
+            console.log('Create Error', createErr);
+            return;
+        }
+
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            instance.requestPaymentMethod(function (err, payload) {
+                if (err) {
+                    console.log('Request Payment Method Error', err);
+                    return;
+                }
+
+                // Add the nonce to the form and submit
+                document.querySelector('#nonce').value = payload.nonce;
+                form.submit();
+            });
+        });
+    });
 });
-});
-
-var aptId = $('#apt-id').val();
-console.log(aptId);
